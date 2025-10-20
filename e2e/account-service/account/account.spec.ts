fad2d320a34a71faef1ddb9418a8aaa0921a3ce3
Jimena Nemiña <jimenanemina@Jimenas-MacBook-Pro.local>
2025-10-20 14:12:18 -0300

    Added scenarios for get account and change photo

---
+++ e2e/account-service/account/account.spec.ts
@@ -0,0 +1,90 @@
+import { test, expect } from "@playwright/test";
+import { ApiClient } from "../../../utils/apiClient";
+import { validateAccountResponse } from "../../../utils/schemaValidator";
+import { AccountResponse } from "../../../utils/types";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
+
+const baseURL = process.env.API_BASE_URL!;
+const email = process.env.API_EMAIL!;
+const password = process.env.API_PASSWORD!;
+const accountEndpoint = process.env.API_ACCOUNT_URL!;
+const accountPhotoEndpoint = process.env.API_PHOTO_URL!;
+
+test.describe("Account Service - GET Account Info", () => {
+  let apiClient: ApiClient;
+
+  test.beforeAll(async () => {
+    apiClient = new ApiClient(baseURL);
+    await apiClient.init();
+    await apiClient.authenticate(email, password);
+  });
+
+  test.afterAll(async () => {
+    await apiClient.dispose();
+  });
+
+  test("GET /account - Should return valid account information", async () => {
+    const response = await apiClient.get(accountEndpoint);
+    expect(response.status(), "Expected 200 OK for valid token").toBe(200);
+
+    const body: unknown = await response.json();
+    validateAccountResponse(body);
+
+    const account = (body as AccountResponse).data.accountInfo;
+    expect(account.email).toBe(email);
+    expect(account.deviceUserId).toBeDefined();
+  });
+
+  test("GET /account - Should return 401 Unauthorized with invalid token", async () => {
+    (apiClient as any).token = "invalid-token-12345";
+
+    const response = await apiClient.get(accountEndpoint);
+    expect(response.status(), "Expected 401 for invalid token").toBe(401);
+
+    await apiClient.authenticate(email, password);
+  });
+
+  test("GET /account - Should throw an error when no token is provided", async () => {
+    (apiClient as any).token = null;
+
+    await expect(async () => {
+      await apiClient.get(accountEndpoint);
+    }).rejects.toThrow("Token is not set");
+
+    await apiClient.authenticate(email, password);
+  });
+});
+
+
+test.describe("Account service - Change account user photo", () => {
+    let apiClient: ApiClient;
+
+    test.beforeAll(async () => {
+        apiClient = new ApiClient(baseURL);
+        await apiClient.init();
+        await apiClient.authenticate(email, password);
+    });
+
+    test.afterAll(async () => {
+        await apiClient.dispose();
+    });
+
+    test("POST /account/photo - Should change photo correctly", async () => {
+
+    const filePath = path.resolve(__dirname, "../../../assets/highQFitnessCopy.jpg");
+    const response = await apiClient.postMultipart(accountPhotoEndpoint, {
+    fieldName: "photo",
+    filePath,
+});
+
+
+  expect(response.status(), "Expected 201 for successful photo upload").toBe(201);
+
+  const json = await response.json();
+  console.log("Response JSON:", json);
+});
+
+});
--- utils/apiClient.ts
+++ utils/apiClient.ts
@@ -1,5 +1,7 @@
 import { APIRequestContext, request, APIResponse } from "@playwright/test";
 import dotenv from "dotenv";
+import path from "path";
+import * as fs from "fs";
 
 dotenv.config();
 
@@ -58,6 +60,15 @@ export class ApiClient {
     if (!this.token)
       throw new Error("Auth failed: no access token found in response");
   }
+  
+  private getHeaders(contentType = "application/json"): Record<string, string> {
+    if (!this.token) throw new Error("Token is not set");
+
+    return {
+      Authorization: `Bearer ${this.token}`,
+      ...(contentType ? { "Content-Type": contentType } : {}),
+    };
+  }
 
   async get(endpoint: string): Promise<APIResponse> {
     if (!this.token) {
@@ -88,4 +99,48 @@ export class ApiClient {
 
     return response;
   }
+
+  async post(endpoint: string, body?: object): Promise<Response> {
+    return fetch(`${this.baseURL}${endpoint}`, {
+      method: "POST",
+      headers: this.getHeaders(),
+      body: body ? JSON.stringify(body) : undefined,
+    });
+  }
+
+async postMultipart(
+  endpoint: string,
+  { fieldName, filePath }: { fieldName: string; filePath: string }
+): Promise<APIResponse> {
+  if (!this.token) throw new Error("Token is not set");
+  if (!fs.existsSync(filePath)) throw new Error(`File not found: ${filePath}`);
+
+  const fileBuffer = fs.readFileSync(filePath);
+  const fileName = path.basename(filePath);
+
+  const response = await this.apiContext.post(endpoint, {
+    headers: {
+      Authorization: `Bearer ${this.token}`,
+    },
+    multipart: {
+      [fieldName]: {
+        name: fileName,
+        mimeType: "image/jpeg",
+        buffer: fileBuffer,
+      },
+    },
+  });
+
+  if (!response.ok()) {
+    console.error(
+      `Upload failed: ${response.status()} ${response.statusText()}`
+    );
+    console.log(await response.text());
+  } else {
+    console.log("Photo uploaded successfully!");
+  }
+
+  return response;
+}
+
 }
