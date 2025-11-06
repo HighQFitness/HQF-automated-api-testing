678890a4933fd4f6d3dea41c406a59f84676e3c4
Jimena Nemiña <jimenanemina@gmail.com>
2025-11-06 15:54:57 -0300

    added pathc scenarios with invalid and missing token

--- e2e/account-service/notificationpreferences/notificationPrefences.spec.ts
+++ e2e/account-service/notificationpreferences/notificationPrefences.spec.ts
@@ -1,14 +1,18 @@
 import { test, expect } from "@playwright/test";
 import { ApiClient } from "../../../utils/apiClient";
 import { validateNotificationPreferencesResponse } from "../../../utils/schemaValidator";
-import { HealthInfoResponse, NotificationPreferencesResponse } from "../../../utils/types";
+import {
+  HealthInfoResponse,
+  NotificationPreferencesResponse,
+} from "../../../utils/types";
 import dotenv from "dotenv";
 import { NotificationPreferencesFactory } from "../../../utils/notificationPreferenceDataFactory";
 
 dotenv.config();
 
 const baseURL = process.env.API_BASE_URL!;
-const notificationPreferencesEndpoint = process.env.API_NOTIFICATION_PREFERENCES_URL!;
+const notificationPreferencesEndpoint =
+  process.env.API_NOTIFICATION_PREFERENCES_URL!;
 
 test.describe("Account Service - GET Notifications preferences", () => {
   let apiClient: ApiClient;
@@ -33,24 +37,34 @@ test.describe("Account Service - GET Notifications preferences", () => {
     const actual = (body as NotificationPreferencesResponse).data;
 
     expected.preferences.forEach(
-    (pref: { notificationCategory: unknown; notificationOption: unknown }, index: number) => {
-      expect(actual.preferences[index].notificationCategory).toBe(pref.notificationCategory);
-      expect(actual.preferences[index].notificationOption).toBe(pref.notificationOption);
-    }
-  );
-});
+      (
+        pref: { notificationCategory: unknown; notificationOption: unknown },
+        index: number
+      ) => {
+        expect(actual.preferences[index].notificationCategory).toBe(
+          pref.notificationCategory
+        );
+        expect(actual.preferences[index].notificationOption).toBe(
+          pref.notificationOption
+        );
+      }
+    );
+  });
 
   test("GET /notification-preferences - Should return 401 Unauthorized with invalid token", async () => {
     (apiClient as any).token = "invalid-token-12345";
-    const response = await apiClient.get(notificationPreferencesEndpoint, false);
+    const response = await apiClient.get(
+      notificationPreferencesEndpoint,
+      false
+    );
     expect(response.status()).toBe(401);
   });
 
   test("GET /notification-preferences - Should throw when no token is provided", async () => {
     (apiClient as any).token = null;
-    await expect(apiClient.get(notificationPreferencesEndpoint, false)).rejects.toThrow(
-      "Token is not set"
-    );
+    await expect(
+      apiClient.get(notificationPreferencesEndpoint, false)
+    ).rejects.toThrow("Token is not set");
   });
 });
 
@@ -67,9 +81,13 @@ test.describe("Account Service - PATCH Notification Preferences", () => {
   });
 
   test("PATCH /notification-preferences - Should return valid updated notification preferences", async () => {
-      const payload = NotificationPreferencesFactory.returnValidPreferences();
+    const payload = NotificationPreferencesFactory.returnValidPreferences();
 
-    const response = await apiClient.patch(notificationPreferencesEndpoint, payload, true);
+    const response = await apiClient.patch(
+      notificationPreferencesEndpoint,
+      payload,
+      true
+    );
     expect(response.status(), "Expected 200 OK for valid token").toBe(200);
 
     const body = (await response.json()) as NotificationPreferencesResponse;
@@ -78,27 +96,34 @@ test.describe("Account Service - PATCH Notification Preferences", () => {
     expect(Array.isArray(notificationPreferences)).toBe(true);
     expect(notificationPreferences.length).toBeGreaterThan(0);
 
-    
     payload.preferences.forEach((pref, index) => {
-      expect(notificationPreferences[index].notificationCategory).toBe(pref.notificationCategory);
-      expect(notificationPreferences[index].notificationOption).toBe(pref.notificationOption);
+      expect(notificationPreferences[index].notificationCategory).toBe(
+        pref.notificationCategory
+      );
+      expect(notificationPreferences[index].notificationOption).toBe(
+        pref.notificationOption
+      );
     });
   });
 
-  // test("PATCH /notification-preferences - Should return 401 Unauthorized with invalid token", async () => {
-  //   (apiClient as any).token = "invalid-token-12345";
-  //   const payload = HealthInfoFactory.valid();
+  test("PATCH /notification-preferences - Should return 401 Unauthorized with invalid token", async () => {
+    (apiClient as any).token = "invalid-token-12345";
+    const payload = NotificationPreferencesFactory.returnValidPreferences();
 
-  //   const response = await apiClient.patch(healthInfoEndpoint, payload, false);
-  //   expect(response.status()).toBe(401);
-  // });
+    const response = await apiClient.patch(
+      notificationPreferencesEndpoint,
+      payload,
+      false
+    );
+    expect(response.status()).toBe(401);
+  });
 
-  // test("PATCH /notification-preferences - Should throw when no token is provided", async () => {
-  //   (apiClient as any).token = null;
-  //   const payload = HealthInfoFactory.valid();
+  test("PATCH /notification-preferences - Should throw when no token is provided", async () => {
+    (apiClient as any).token = null;
+    const payload = NotificationPreferencesFactory.returnValidPreferences();
 
-  //   await expect(apiClient.patch(healthInfoEndpoint,payload, false)).rejects.toThrow(
-  //     "Token is not set"
-  //   );
-  // });
+    await expect(
+      apiClient.patch(notificationPreferencesEndpoint, payload, false)
+    ).rejects.toThrow("Token is not set");
+  });
 });
